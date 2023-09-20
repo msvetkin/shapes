92b696fe0cc74fa77dac46a70b26218079070be3
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-09-20 15:51:58 +0200

    feat(cli): add small shape application

--- src/cli/src/main.cpp
+++ src/cli/src/main.cpp
@@ -4,8 +4,24 @@
 #include "shapes/core/shape.hpp"
 
 #include <fmt/core.h>
+#include <range/v3/algorithm/for_each.hpp>
+
+struct Rectangle {
+  std::size_t width;
+  std::size_t height;
+};
+
+void draw(const Rectangle &rectangle) {
+  fmt::print("draw rectangle: {}, {}\n", rectangle.width, rectangle.height);
+}
 
 int main(int /*argc*/, char * /*argv*/ []) {
-  fmt::print("shapes version: {}\n", shapes::core::version());
+  using namespace shapes::core;
+
+  std::vector<Shape> shapes;
+  shapes.emplace_back(Rectangle{10, 20});
+
+
+  ranges::for_each(shapes, shapes::core::draw);
   return 0;
 }
