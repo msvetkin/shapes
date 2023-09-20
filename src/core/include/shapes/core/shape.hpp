2a9c9cebeeb55d76ec5ac73628c69c9e4e7fb83b
Mikhail Svetkin <mikhail.svetkin@gmail.com>
2023-09-20 15:51:53 +0200

    feat(core): add basic type erased shape

--- src/core/include/shapes/core/shape.hpp
+++ src/core/include/shapes/core/shape.hpp
@@ -5,10 +5,60 @@
 
 #include "shapes/core/export.hpp"
 
+#include <fmt/core.h>
+
 #include <string>
+#include <utility>
+#include <memory>
 
 namespace shapes::core {
 
-[[nodiscard]] SHAPES_CORE_EXPORT std::string version() noexcept;
+class SHAPES_CORE_EXPORT Shape {
+  struct Concept {
+    virtual ~Concept() = default;
+
+    virtual void do_draw() const = 0;
+    virtual std::unique_ptr<Concept> clone() const = 0;
+  };
+
+  template<typename T>
+  struct Model : Concept {
+    explicit Model(T shape)
+      : shape_(std::move(shape)) {
+
+    }
+
+    void do_draw() const override {
+      fmt::print("do_draw, {}\n", __PRETTY_FUNCTION__);
+      draw(shape_);
+    }
+
+    std::unique_ptr<Concept> clone() const override {
+      return std::make_unique<Model<T>>(*this);
+    }
+
+    T shape_;
+  };
+
+  friend void draw(const Shape &shape);
+
+ public:
+  template<typename T>
+  explicit Shape(T shape)
+    : pimpl_(std::make_unique<Model<T>>(std::move(shape))) {
+    fmt::print("Shape: {}\n", __PRETTY_FUNCTION__);
+  }
+
+  Shape(const Shape &other);
+  Shape& operator=(const Shape &other);
+
+  Shape(Shape &&other) = default;
+  Shape& operator=(Shape &&other) = default;
+
+ private:
+  std::unique_ptr<Concept> pimpl_;
+};
+
+SHAPES_CORE_EXPORT void draw(const Shape &shape);
 
 } // namespace shapes::core
--- src/core/src/shape.cpp
+++ src/core/src/shape.cpp
@@ -3,10 +3,22 @@
 
 #include "shapes/core/shape.hpp"
 
+#include <fmt/core.h>
+
 namespace shapes::core {
 
-std::string version() noexcept {
-  return "0.0.1";
+void draw(const Shape &shape) {
+  fmt::print("shapes::core::draw\n");
+  shape.pimpl_->do_draw();
+}
+
+Shape::Shape(const Shape &other)
+ : pimpl_(other.pimpl_->clone()) {
+}
+
+Shape& Shape::operator=(const Shape &other) {
+  other.pimpl_->clone().swap(pimpl_);
+  return *this;
 }
 
 } // namespace shapes::core
